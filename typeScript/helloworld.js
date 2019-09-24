71b8d13a8ed4fa56b0947f8a62025ce4dc253020
lia07 <sarzurisilvestre@example.com>
2019-09-24 15:23:45 +0200

    correccion helloworld

---
+++ typeScript/helloworld.js
@@ -0,0 +1,18 @@
+
+export.__esModule = true;
+var Person = /**@class */ (function(){
+    function Person(){
+    }
+    return Person;
+}());
+export.Person = Person;
+var gss ={
+    firstName: "Gladys",
+    lastName: "Sarzuri"
+};
+var ech = {
+    firstName: "Erlinda",
+    lastName: "Chambi"
+};
+console.log(gss);
+console.log(ech);
--- typeScript/helloworld.ts
+++ typeScript/helloworld.ts
@@ -1,3 +1,14 @@
-interface Person{
-    
+export class Person{
+    firstName: string;
+    lastName: string;
 }
+var gss: Person = {
+    firstName: "Gladys",
+    lastName: "Sarzuri"
+};
+var ech: Person = {
+    firstName: "Erlinda",
+    lastName: "Chambi"
+};
+console.log(gss);
+console.log(ech);
