5d813c69b835f1223ea267c774be44d885700bfb
lia07 <sarzurisilvestre@example.com>
2019-09-24 16:37:31 +0200

    correccion variables.ts

--- typeScript/variables.js
+++ typeScript/variables.js
@@ -7,3 +7,15 @@ var greeting = "hello  " + firstName;
 console.log(greeting);
 var list = [3, 7];
 console.log(list);
+var nombre = "Gladys Sarzuri";
+function ejemplo() {
+    var x = 100;
+    for (var m = 0; m < 10; m++) {
+        var y = 200;
+        console.log("variable x dentro del for", x);
+        console.log("variable x dentro del for", y);
+    }
+    console.log("variable x fuera del for", x);
+    console.log("variable y fuera del for", y);
+}
+ejemplo();
--- typeScript/variables.ts
+++ typeScript/variables.ts
@@ -9,3 +9,16 @@ console.log(greeting);
 
 let  list:Array<number> = [3,7];
 console.log(list);
+
+let nombre = "Gladys Sarzuri";
+function ejemplo(){
+    let x: number = 100;
+    for(let m=0; m<10;m++){
+        var y=200;
+        console.log("variable x dentro del for", x);
+        console.log("variable x dentro del for", y);
+    }
+    console.log("variable x fuera del for", x);
+    console.log("variable y fuera del for", y);
+}
+ejemplo()
