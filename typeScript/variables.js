99cb812297fca8bf8ec1e640579ff93a91bdc07b
lia07 <sarzurisilvestre@example.com>
2019-09-24 11:57:57 +0200

    variables

---
+++ typeScript/variables.js
@@ -0,0 +1,9 @@
+var disabled = true; //true or false
+console.log(disabled);
+var firstName = "angel";
+console.log(firstName);
+//baccting por que sus comillas son al otro lado
+var greeting = "hello  " + firstName;
+console.log(greeting);
+var list = [3, 7];
+console.log(list);
---
+++ typeScript/variables.ts
@@ -0,0 +1,11 @@
+let disabled:boolean = true; //true or false
+console.log(disabled);
+let  firstName: string = "angel";
+console.log(firstName);
+
+//baccting por que sus comillas son al otro lado
+let greeting:string = `hello  ${firstName}`;
+console.log(greeting);
+
+let  list:Array<number> = [3,7];
+console.log(list);
