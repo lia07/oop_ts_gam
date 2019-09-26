5aa90f5feafdfed2ceabae14f6fe800f655cb42e
lia07 <sarzurisilvestre@example.com>
2019-09-26 09:46:57 +0200

    creacion de main.ts en shapes

---
+++ shapes/main.js
@@ -0,0 +1,41 @@
+"use strict";
+exports.__esModule = true;
+/**
+ * clase
+ */
+var Rectangulo = /** @class */ (function () {
+    function Rectangulo(px, py, h, w) {
+        this.posX = px;
+        this.posY = py;
+        this.heigth = h;
+        this.width = w;
+    }
+    Rectangulo.prototype.setPosX = function (px) {
+        this.posX = px;
+    };
+    Rectangulo.prototype.getPosX = function () {
+        return this.posX;
+    };
+    Rectangulo.prototype.setPosY = function (py) {
+        this.posY = py;
+    };
+    Rectangulo.prototype.getPosY = function () {
+        return this.posY;
+    };
+    Rectangulo.prototype.setHeigth = function (h) {
+        this.heigth = h;
+    };
+    Rectangulo.prototype.getHeigth = function () {
+        return this.heigth;
+    };
+    return Rectangulo;
+}());
+exports.Rectangulo = Rectangulo;
+/**
+ * pruebas
+ */
+var posx1 = new Pos(2, 5);
+var posx2 = new Pos(1, 3);
+posx1.setPosX(2);
+console.log(posx1);
+console.log(posx2);
---
+++ shapes/main.ts
@@ -0,0 +1,43 @@
+/**
+ * clase
+ */
+export class Rectangulo{
+    private posX: number;
+    private posY: number;
+    private heigth: number;
+    private width: number;
+
+    public constructor(px: number, py: number, h: number, w: number){
+        this.posX = px;
+        this.posY = py;
+        this.heigth = h;
+        this.width = w;
+    }
+
+    public setPosX( px: number):void{
+        this.posX = px;
+    }
+    public getPosX(): number{
+       return  this.posX;
+    }
+    public setPosY( py: number):void{
+        this.posY = py;
+    }
+    public getPosY(): number{
+       return  this.posY;
+    }
+    public setHeigth( h: number):void{
+        this.heigth = h;
+    }
+    public getHeigth(): number{
+       return  this.heigth;
+    }
+}
+ /**
+  * pruebas
+  */
+let posx1: Rectangulo = new Pos(2, 5);
+let posx2: Rectangulo = new Pos(1,3); 
+posx1.setPosX(2);
+console.log(posx1);
+console.log(posx2);
