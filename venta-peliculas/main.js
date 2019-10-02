39df8c4fbaa388ef9191adee588d74532b6c97d4
lia07 <sarzurisilvestre@example.com>
2019-10-02 08:41:43 +0200

    pruebas

--- venta-peliculas/main.js
+++ venta-peliculas/main.js
@@ -23,37 +23,39 @@ exports.__esModule = true;
  *  deben guardar en una lista.
  */
 var Disco = /** @class */ (function () {
-    function Disco(nom, pr) {
+    function Disco(nom, precio, categ) {
         this.nombre = nom;
-        this.precio = pr;
+        this.precio = precio;
+        this.categoria = categ;
     }
     return Disco;
 }());
 exports.Disco = Disco;
 var Cd = /** @class */ (function (_super) {
     __extends(Cd, _super);
-    function Cd(nom, pr) {
-        return _super.call(this, nom, pr) || this;
+    function Cd(nom, pr, categ) {
+        return _super.call(this, nom, pr, categ) || this;
     }
     return Cd;
 }(Disco));
 exports.Cd = Cd;
 var Dvd = /** @class */ (function (_super) {
     __extends(Dvd, _super);
-    function Dvd(nom, pr) {
-        return _super.call(this, nom, pr) || this;
+    function Dvd(nom, precio, categ) {
+        return _super.call(this, nom, precio, categ) || this;
     }
     return Dvd;
 }(Disco));
 exports.Dvd = Dvd;
-var Bluray = /** @class */ (function (_super) {
-    __extends(Bluray, _super);
-    function Bluray(nom, pr) {
-        return _super.call(this, nom, pr) || this;
+var BluRay = /** @class */ (function (_super) {
+    __extends(BluRay, _super);
+    function BluRay(nom, pr, categ) {
+        return _super.call(this, nom, pr, categ) || this;
     }
-    return Bluray;
+    return BluRay;
 }(Disco));
-exports.Bluray = Bluray;
+exports.BluRay = BluRay;
+/*********************************************************************/
 var Cliente = /** @class */ (function () {
     function Cliente(nom, ap, ed) {
         this.nombre = nom;
@@ -63,17 +65,102 @@ var Cliente = /** @class */ (function () {
     return Cliente;
 }());
 exports.Cliente = Cliente;
-var TiendaDisco = /** @class */ (function () {
-    function TiendaDisco(cu, est, fe) {
-        this.curso = cu;
-    }
-    return TiendaDisco;
-}());
-exports.TiendaDisco = TiendaDisco;
+var cliente1 = new Cliente("Gladys", "Sarzuri", 26);
+var cliente2 = new Cliente("Deysi", "Sarzuri", 24);
+var cliente3 = new Cliente("Monica", "Sarzuri", 28);
+console.log(cliente1, cliente2, cliente3);
+var dumbo = new Dvd("Dumbo", 10, "infantil"); //Dvd
+var rambo = new Cd("Rambo", 20, "accion");
+var mypelicula = new Cd("pelicula", 80, "Terror");
+var hachiko = new BluRay("hachiko", 8, "familiar");
+var pinocho = new Dvd("hachiko", 10, "familiar"); //Dvd
+console.log(dumbo, rambo, mypelicula, hachiko, pinocho);
+/***************************************************************************/
 var VentaDisco = /** @class */ (function () {
-    function VentaDisco(cu, est, fe) {
-        this.curso = cu;
+    function VentaDisco(producto, cliente) {
+        this.producto = producto;
+        this.cliente = cliente;
     }
+    VentaDisco.prototype.montoDeLaVenta = function () {
+        var monto = this.producto.precio;
+        console.log(monto);
+        return monto;
+    };
     return VentaDisco;
 }());
 exports.VentaDisco = VentaDisco;
+var venta1 = new VentaDisco(dumbo, cliente1);
+var venta2 = new VentaDisco(rambo, cliente2);
+var venta3 = new VentaDisco(hachiko, cliente3);
+var venta4 = new VentaDisco(pinocho, cliente1);
+var venta5 = new VentaDisco(mypelicula, cliente3);
+/*******************************************************************/
+var TiendaDisco = /** @class */ (function () {
+    function TiendaDisco(nombre) {
+        this.nombreTienda = nombre;
+        this.ventas = [];
+    }
+    TiendaDisco.prototype.realizarVenta = function (venta) {
+        this.ventas.push(venta);
+    };
+    TiendaDisco.prototype.calcularVentaCds = function () {
+        var suma = 0;
+        for (var i = 0; i < this.ventas.length; i++) {
+            var prod = this.ventas[i].producto;
+            if (prod instanceof Cd) {
+                suma += prod.precio;
+            }
+        }
+        return suma;
+    };
+    TiendaDisco.prototype.calcularVentaDvds = function () {
+        var suma = 0;
+        for (var i = 0; i < this.ventas.length; i++) {
+            var prod = this.ventas[i].producto;
+            if (prod instanceof Dvd) {
+                suma += prod.precio;
+            }
+        }
+        return suma;
+    };
+    TiendaDisco.prototype.calcularVentaBluRay = function () {
+        var suma = 0;
+        for (var i = 0; i < this.ventas.length; i++) {
+            var prod = this.ventas[i].producto;
+            if (prod instanceof BluRay) {
+                suma += prod.precio;
+            }
+        }
+        return suma;
+    };
+    TiendaDisco.prototype.totalVenta = function () {
+        var suma = 0;
+        for (var i = 0; i < this.ventas.length; i++) {
+            var prod = this.ventas[i].producto;
+            if (prod instanceof Dvd) {
+                suma += prod.precio;
+            }
+        }
+        return suma;
+    };
+    return TiendaDisco;
+}());
+exports.TiendaDisco = TiendaDisco;
+/*********/
+var tienda1 = new TiendaDisco("MiTienda");
+tienda1.realizarVenta(venta1);
+tienda1.realizarVenta(venta2);
+tienda1.realizarVenta(venta3);
+tienda1.realizarVenta(venta4);
+tienda1.realizarVenta(venta5);
+// let tienda2: TiendaDisco = new TiendaDisco("Su Tienda");
+// tienda2.realizarVenta(venta2);
+// let tienda3: TiendaDisco = new TiendaDisco("Center");
+// tienda3.realizarVenta(venta3);
+var ingresosCds = tienda1.calcularVentaCds();
+console.log("Los ingrsos de la venta de Cds" + ingresosCds + " Bs");
+var ingresosDvds = tienda1.calcularVentaDvds();
+console.log("Los ingrsos de la venta de Dvds" + ingresosDvds + " Bs");
+var ingresosBluRays = tienda1.calcularVentaBluRay();
+console.log("Los ingrsos de la venta de BluRay" + ingresosBluRays + " Bs");
+console.log("Total Venta", tienda1.totalVenta());
--- venta-peliculas/main.ts
+++ venta-peliculas/main.ts
@@ -9,34 +9,35 @@
  */
 export class Disco{
     private nombre: string; //atributos
-    private precio: number;
-    private formato: string;
+    //private precio: number;
+    precio: number;
+    private categoria: string;
 
-    constructor(nom: string, pr: number, fr: string ){
+    constructor(nom: string, precio: number, categ: string ){
         this.nombre= nom;
-        this.precio = pr;
-        this.formato =fr;
+        this.precio = precio;
+        this.categoria =categ;
    }
 }
 export class Cd extends Disco{
 
-    constructor(nom: string, pr: number, fr: string ){
-        super(nom, pr, fr);
+    constructor(nom: string, pr: number, categ: string ){
+        super(nom, pr, categ);
    }
 }
 export class Dvd extends Disco{
 
-    constructor(nom: string, pr: number, fr: string){
-        super(nom, pr, fr);
+    constructor(nom: string, precio: number, categ: string){
+        super(nom, precio, categ);
    }
 }
-export class Bluray extends Disco{
+export class BluRay extends Disco{
 
-    constructor(nom: string, pr: number, fr: string){
-        super(nom, pr, fr);
+    constructor(nom: string, pr: number, categ: string){
+        super(nom, pr, categ);
    }
 }
-/*************/
+/*********************************************************************/
 export class Cliente{
     private nombre: string; //atributos
     private apellido: string;
@@ -48,32 +49,111 @@ export class Cliente{
         this.edad = ed;
    }
 }
+let cliente1: Cliente = new Cliente("Gladys","Sarzuri", 26);
+let cliente2: Cliente = new Cliente("Deysi","Sarzuri", 24);
+let cliente3: Cliente = new Cliente("Monica","Sarzuri", 28);
+console.log(cliente1,cliente2, cliente3);
 
-export class TiendaDisco{
-    private disco: Disco[];//atributos
-   // private cantidad : number;  
-    
-    constructor(dis: Disco[]){
-        this.disco = dis;
+let dumbo: Dvd = new Dvd("Dumbo",10,"infantil");//Dvd
+let rambo: Cd = new Cd("Rambo",20,"accion");
+let mypelicula: Cd = new Cd("pelicula", 80, "Terror");
+let hachiko: BluRay = new BluRay("hachiko", 8,"familiar");
+let pinocho: Dvd = new Dvd("hachiko", 10,"familiar");//Dvd
+console.log(dumbo, rambo, mypelicula, hachiko, pinocho);
 
-   }
-   public setAgregarDisco(disco1: Disco): void{
-    this.disco.push(disco1);
-   }
-}
+/***************************************************************************/
 export class VentaDisco{
-    private tipo: Disco[]; //atributos
-    private fecha: string;
+   // private producto: Disco;
+   producto: Disco;
+    private cliente: Cliente;
     
-    constructor(fe: string){
-        this.fecha =fe;
+    public constructor(producto: Disco, cliente: Cliente){
+        this.producto=producto;
+        this.cliente=cliente;
    }
-   public setVentaDisco(cantidad: number, dis: Disco){
-    this.disco = ;
+   public montoDeLaVenta(): number{
+       let monto=this.producto.precio;
+       console.log(monto);
+       return monto;
    }
 }
-/**
- * Prueba
- */
-let disco1: Disco = new Disco("mipelicula",25, "ext2");
-let dir: Disco[] = [disco1];
+
+let venta1: VentaDisco = new VentaDisco(dumbo, cliente1);
+let venta2: VentaDisco = new VentaDisco(rambo, cliente2);
+let venta3: VentaDisco = new VentaDisco(hachiko, cliente3);
+let venta4: VentaDisco = new VentaDisco(pinocho, cliente1);
+let venta5: VentaDisco = new VentaDisco(mypelicula, cliente3);
+
+/*******************************************************************/
+export class TiendaDisco{
+    private ventas: VentaDisco[];
+    private nombreTienda: string;
+    
+    constructor(nombre: string){
+        this.nombreTienda = nombre;
+        this.ventas = [];
+   }
+   realizarVenta(venta: VentaDisco): void{
+       this.ventas.push(venta);
+   }
+   calcularVentaCds(): number{
+       let suma = 0;
+       for(let i =0; i<this.ventas.length; i++){
+           let prod=this.ventas[i].producto;
+           if(prod instanceof Cd){
+               suma+=prod.precio;
+           }
+       }
+       return suma;
+   }
+   calcularVentaDvds(): number{
+       let suma = 0;
+       for(let i = 0; i<this.ventas.length; i++){
+           let prod = this.ventas[i].producto;
+           if(prod instanceof Dvd){
+               suma+=prod.precio;
+           }
+       }
+       return suma;
+   }
+   calcularVentaBluRay(): number{
+    let suma = 0;
+    for(let i = 0; i<this.ventas.length; i++){
+        let prod = this.ventas[i].producto;
+        if(prod instanceof BluRay){
+            suma+=prod.precio;
+        }
+    }
+    return suma;
+    }
+    totalVenta(): number{
+        let suma = 0;
+        for(let i = 0; i<this.ventas.length; i++){
+            let prod = this.ventas[i].producto;
+            if(prod instanceof Dvd){
+                suma+=prod.precio;
+            }
+        }
+        return suma;
+    }
+}
+/*********/
+let tienda1: TiendaDisco = new TiendaDisco("MiTienda");
+tienda1.realizarVenta(venta1);
+tienda1.realizarVenta(venta2);
+tienda1.realizarVenta(venta3);
+tienda1.realizarVenta(venta4);
+tienda1.realizarVenta(venta5);
+// let tienda2: TiendaDisco = new TiendaDisco("Su Tienda");
+// tienda2.realizarVenta(venta2);
+// let tienda3: TiendaDisco = new TiendaDisco("Center");
+// tienda3.realizarVenta(venta3);
+
+let ingresosCds=tienda1.calcularVentaCds();
+console.log("Los ingrsos de la venta de Cds" + ingresosCds + " Bs");
+let ingresosDvds=tienda1.calcularVentaDvds();
+console.log("Los ingrsos de la venta de Dvds" + ingresosDvds + " Bs");
+let ingresosBluRays = tienda1.calcularVentaBluRay();
+console.log("Los ingrsos de la venta de BluRay" + ingresosBluRays + " Bs");
+
+console.log("Total Venta", tienda1.totalVenta());
